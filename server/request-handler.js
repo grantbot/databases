a8d1cafee1f52cd2409298b29d5bfb9e0b4ebaa4
grantbot <grantwu2@gmail.com>
2014-11-21 17:34:42 -0800

    Created table schema

---
+++ server/request-handler.js
@@ -0,0 +1,42 @@
+var utils = require('./utils');
+
+var objectId = 1;
+var messages = [
+  {
+    text: 'hello world',
+    username: 'fred',
+    objectId: objectId
+  }
+];
+
+var actions = {
+  'GET': function(request, response){
+    utils.sendResponse(response, {results: messages});
+  },
+  'POST': function(request, response){
+    utils.collectData(request, function(message){
+      message.objectId = ++objectId;
+      messages.push(message);
+      utils.sendResponse(response, {objectId: 1});
+    });
+  },
+  'OPTIONS': function(request, response){
+    utils.sendResponse(response);
+  }
+};
+
+module.exports = function(request, response) {
+  var action = actions[request.method];
+  if( action ){
+    action(request, response);
+  } else {
+    utils.sendResponse(response, "Not Found", 404);
+  }
+};
+
+
+
+
+
+
+
--- server/schema.sql
+++ server/schema.sql
@@ -3,7 +3,12 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT(3) NOT NULL auto_increment,
+  username VARCHAR(50) NOT NULL,
+  message VARCHAR(150) NOT NULL,
+  created TIMESTAMP(6) NOT NULL,
+  room VARCHAR(20) NOT NULL,
+  PRIMARY KEY(id)
 );
 
 /* Create other tables and define schemas for them here! */
